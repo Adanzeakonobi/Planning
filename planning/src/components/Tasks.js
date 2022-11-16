e862e1d30d33526e0b87c396e3a70239fe8aca08
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-16 11:59:15 -0600

    Make the list of task array part of the Tasks component

--- planning/src/components/Tasks.js
+++ planning/src/components/Tasks.js
@@ -1,31 +1,32 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Study React',
-        day: '28th October 2022 by 6:30PM',
-        reminder: 'true'
-    },
-    {
-        id: 2,
-        text: 'Study Ruby on rails',
-        day: '26th October 2022 by 6:30PM',
-        reminder: 'true'
-    },
-    {
-        id: 3,
-        text: 'Go to the market',
-        day: '25th October 2022 by 6:30PM',
-        reminder: 'false'
-    },
-    {
-        id: 4,
-        text: 'Time to cook',
-        day: '24th October 2022 by 6:30PM',
-        reminder: 'true'
-    }
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [ tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Study React',
+            day: '28th October 2022 by 6:30PM',
+            reminder: 'true'
+        },
+        {
+            id: 2,
+            text: 'Study Ruby on rails',
+            day: '26th October 2022 by 6:30PM',
+            reminder: 'true'
+        },
+        {
+            id: 3,
+            text: 'Go to the market',
+            day: '25th October 2022 by 6:30PM',
+            reminder: 'false'
+        },
+        {
+            id: 4,
+            text: 'Time to cook',
+            day: '24th October 2022 by 6:30PM',
+            reminder: 'true'
+        }
+    ])
     return(
         <div>
             {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
