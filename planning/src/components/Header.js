800607ff3842f69c922ee59d70a5de1e0b6fd5bb
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-16 11:37:51 -0600

    Update button function

--- planning/src/components/Header.js
+++ planning/src/components/Header.js
@@ -1,22 +1,26 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import Button from './Button';
+import Button from './Button'
 
 const Header = ({ title }) => {
+
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
-    <Header className="header">
+    <header className='header'>
       <h1>{title}</h1>
-      <Button />
-    </Header>
-  );
+      <Button color='purple' text='Add Task' onClick={onClick} />
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-  }
-  
-  Header.propTypes = {
-    title: PropTypes.string.isRequired,
-  }
+  title: 'Task Tracker',
+}
 
-export default Header;
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
