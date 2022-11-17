3164c7133a29951a1d23f18ab1b8351890b283c3
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-17 07:21:51 -0600

    Create delete function

--- planning/src/App.js
+++ planning/src/App.js
@@ -1,12 +1,45 @@
+import { useState } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 const App = () => {
+  const [ tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Study React',
+        day: '28th October 2022 by 6:30PM',
+        reminder: 'true'
+    },
+    {
+        id: 2,
+        text: 'Study Ruby on rails',
+        day: '26th October 2022 by 6:30PM',
+        reminder: 'true'
+    },
+    {
+        id: 3,
+        text: 'Go to the market',
+        day: '25th October 2022 by 6:30PM',
+        reminder: 'false'
+    },
+    {
+        id: 4,
+        text: 'Time to cook',
+        day: '24th October 2022 by 6:30PM',
+        reminder: 'true'
+    }
+])
+
+
+// Delete task
+const deleteTask = (id) => {
+  console.log('delete', id)
+}
   
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- planning/src/components/Tasks.js
+++ planning/src/components/Tasks.js
@@ -1,35 +1,10 @@
-import { useState } from 'react'
+import Task from "./Task";
 
-const Tasks = () => {
-    const [ tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Study React',
-            day: '28th October 2022 by 6:30PM',
-            reminder: 'true'
-        },
-        {
-            id: 2,
-            text: 'Study Ruby on rails',
-            day: '26th October 2022 by 6:30PM',
-            reminder: 'true'
-        },
-        {
-            id: 3,
-            text: 'Go to the market',
-            day: '25th October 2022 by 6:30PM',
-            reminder: 'false'
-        },
-        {
-            id: 4,
-            text: 'Time to cook',
-            day: '24th October 2022 by 6:30PM',
-            reminder: 'true'
-        }
-    ])
+const Tasks = ( {tasks, onDelete} ) => {
+    
     return(
         <div>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {tasks.map((task) => (<Task key={task.id} task={task} onDelete={onDelete}/>))}
         </div>
     )
 }
