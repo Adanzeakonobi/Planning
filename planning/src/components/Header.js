f963c660eb55a2f20a3f8e977222ce4f083dc703
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-21 11:29:58 -0600

    Update the Add task button on the header

--- planning/src/components/Header.js
+++ planning/src/components/Header.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
 
   // const onClick = () => {
   //   console.log('click')
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='purple' text='Add Task' onClick={onAdd} />
+      <Button color={showAdd ? 'red' : 'purple'} text={showAdd ? 'Close' : 'Add Task'} onClick={onAdd} />
     </header>
   )
 }
