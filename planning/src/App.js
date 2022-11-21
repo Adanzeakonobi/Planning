7d0e3c8d88765adda710e533c8095c16d833a7f9
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-21 11:11:38 -0600

    Create a function to add task

--- planning/src/App.js
+++ planning/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () => {
   const [ tasks, setTasks] = useState([
@@ -8,39 +9,51 @@ const App = () => {
         id: 1,
         text: 'Study React',
         day: '28th October 2022 by 6:30PM',
-        reminder: 'true'
+        reminder: true,
     },
     {
         id: 2,
         text: 'Study Ruby on rails',
         day: '26th October 2022 by 6:30PM',
-        reminder: 'true'
+        reminder: true,
     },
     {
         id: 3,
         text: 'Go to the market',
         day: '25th October 2022 by 6:30PM',
-        reminder: 'false'
+        reminder: false,
     },
     {
         id: 4,
         text: 'Time to cook',
         day: '24th October 2022 by 6:30PM',
-        reminder: 'true'
+        reminder: false,
     }
 ])
+
+// Add Task
+const addTask = (task) => {
+  console.log(task)
+}
 
 
 // Delete task
 const deleteTask = (id) => {
-  console.log('delete', id)
+  // console.log('delete', id)
   setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  console.log('toggle', id)
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
 }
   
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks Available'}
+      <AddTask  onAdd={addTask}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks Available')}
     </div>
   );
 }
