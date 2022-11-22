2cc1ad6f595ade7569a51257bc9a0b94ddbb9f33
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-11-22 11:28:29 -0600

    Create a function to link about section to the footer section

---
+++ planning/src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 1.0</h4>
+      <p>Created by Ada Vivian</p>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
